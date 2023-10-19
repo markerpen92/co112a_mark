28396f9189e3dcf23acb23dc287e04761c2dcc25
markerpen92 <markchou920219@gmial.com>
2023-10-19 10:46:39 +0800

    20231019

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,14 +1,58 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// reference https://github.com/ptdriscoll/nand2tetris/blob/master/project4/Fill.asm
+// Understand the reference once and then rewrite it
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+// M-memory   |    D-data    |    A-addr
 
-// Put your code here.
+// step1 - a value '24575' storage in arg 'screenend' {@'value' \n D=A \n @'arg' \n M=D}
+// step2 - loop an adress 'SCEEN adress' in arg 'i' {(LOOP) @'Adress Name' D=A @'arg' M=D}
+// step3 - set white color '0' in 'color adress' {@color M=0}
+// step4 - if condition is true exe code and goto 'SCREENLOOP' {@'keyboard adress' D=M @'loop label' D;JEQ} 
+//         D;JEQ - to check KeyboardAdress has input or not
+// step5 - set white color '1' in 'color adress' {@color M=1}
+// step6 - a SCREELOOP label {(SCREENLOOP)}  end of screenloop?
+// step7 - start painting all pixels {@i D=M @'screenend addr' D=D-M @'Loop addr' D;JGT}    
+//         D;JGT - to check if D is larger than 1 or not
+//         get i addr to memory to data ->  get 'screenend addr' to memory -> data i minus screenend to check is end or not & to data -> if data > 0 means not end
+// step8 - set pixel color {@'color addr' D=M @'SCREEN addr' A=M M=D @'SCREEN addr'=@'SCREEN addr'+1}
+// step9 - define SCREENLOOP {@SCREENLOOP 0;JMP} if 0 end SCREENLOOP
+
+@24575
+D=A
+@screenend
+M=D
+
+(LOOP)
+@screenaddr 
+D=A
+@i
+M=D
+
+@color
+M=0
+
+//@KeyIsInput
+@KBD
+D=M
+
+@SCREENLOOP
+D;JEQ
+@color
+M=-1
+(SCREELOOP)
+
+@i
+D=M
+@screenend
+D=D-M
+@LOOP
+D;JGT
+
+@color
+D=M
+@i
+A=M
+M=D
+M=M+1
+
+@SCREENLOOP
+0;JGT
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// refernece https://github.com/ptdriscoll/nand2tetris/blob/master/project4/Mult.asm
+// Understand the reference once and then rewrite it
+
+// step1 - 
+
+@R0
+D=M
+
+@i
+M=D
+
+@sum
+M=0
+
+(LOOP)
+@i
+D=M
+@STOP
+D;JEQ
+
+@sum
+D=M
+@R1
+D=D+M
+@sum
+M=D
+
+@i
+M=M-1
+
+@LOOP
+0;JMP
+
+(STOP)
+@sum
+D=M
+@R2
+M=D
+
+(END)
+@END
+0;JMP
